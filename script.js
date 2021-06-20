f0777ec504c31dad1b3d907e2a77788b91e23801
Adrian Ionut Zaharia <addyionut@gmail.com>
2021-06-20 11:37:43 +0300

    Update script.js
    - simplified the "checkLetter" function
    - moved the "tries left" message to a new function, where I update the letterCounter and return gameOver when it is the case
    - made one more function where I update the fields and buttons status

--- script.js
+++ script.js
@@ -28,76 +28,67 @@ function replaceCharAt(str,index,chr) {
     return str.substring(0,index) + chr + str.substring(index+1);
 }
 
+function wrongLetter() {
+	letterCounter -= 1;
+	document.getElementById("lettersCounter").innerHTML = "You've got " + letterCounter + " tries left!";
+	if (letterCounter === 0) {
+		return gameOver();
+	}
+}
+
 const usedLetters = document.querySelector('.usedLetters');
 const insertField = document.querySelector('.insertField');
 const check = document.querySelector('.check');
 
 function checkLetter() {
-	document.getElementById("startNewGame").style.visibility = "visible";
-	document.getElementById("start").style.visibility = "hidden";
-	var letterInserted = letterCheck.value.toLowerCase();
-	if (letterInserted.length === 1 || letterInserted.length === lg) {
-		if (letterInserted === wordToGuess) {
-			win();
+	var letterInserted = document.getElementById("letterCheck").value.toLowerCase();
+	if (letterInserted.length !== 1 && letterInserted.length !== lg) {
+		return document.getElementById("lettersCounter").innerHTML = "Wrong input!";
+	}
+	if (!(wordToGuess.includes(letterInserted))) {
+		 wrongLetter();
+	}
+	for (let i = 0; i < lg;) {	
+		if (wordToGuess.charAt(i) === letterInserted) {
+			hiddenWord = replaceCharAt(hiddenWord, i, letterInserted);
+			++i;
 		}
 		else {
-			document.getElementById("usedLetters").innerHTML += letterInserted + " ";
-			for (let i = 0; i < lg;) {	
-				if (wordToGuess.charAt(i) === letterInserted) {
-					hiddenWord = replaceCharAt(hiddenWord, i, letterInserted);
-					++i;
-				}
-				else {
-					++i;
-				}
-			}
-			if (!(wordToGuess.includes(letterInserted))) {
-				letterCounter -= 1;
-			}	
-			document.getElementById("lettersCounter").innerHTML = "You've got " + letterCounter + " tries left!";
-			if (letterCounter === 0) {
-				gameOver();
-			}
-			document.getElementById("printWord").innerHTML = hiddenWord;
-			if (hiddenWord === wordToGuess) {
-				win();
-			}
+			++i;
 		}
-	}
-	else {
-		letterCounter -= 1;
-		document.getElementById("lettersCounter").innerHTML = "You've got " + letterCounter + " tries left!";
+	}	
+	document.getElementById("printWord").innerHTML = hiddenWord;
+	document.getElementById("usedLetters").innerHTML += letterInserted + " ";
+	if (letterInserted === wordToGuess || hiddenWord === wordToGuess) {
+		return win();
 	}
 }
-function win() {
-	document.getElementById("startNewGame").style.visibility = "visible";
-	document.getElementById("start").style.visibility = "hidden";
-	document.getElementById("usedLetters").innerHTML = "";
-	document.getElementById("win||gameOver").innerHTML = "Congratulations! You guessed it!";
-	document.getElementById("wordToGuess").innerHTML = "";
-	document.getElementById("lettersCounter").innerHTML = "";
+
+function fieldsStatus() {
 	insertField.disabled = true;
 	check.disabled = true;
+	document.getElementById("usedLetters").innerHTML = "";
+	document.getElementById("lettersCounter").innerHTML = "";
+}
+
+function win() {
+	fieldsStatus();
+	document.getElementById("winMessageGO").innerHTML = "<strong>Congratulations! You guessed it!</strong>";
+	document.getElementById("wordToGuess").innerHTML = "";
 }
 
 function gameOver() {
-	document.getElementById("startNewGame").style.visibility = "visible";
-	document.getElementById("start").style.visibility = "hidden";
-	insertField.disabled = true;
-	check.disabled = true;
-	document.getElementById("usedLetters").innerHTML = "";
-	document.getElementById("win||gameOver").innerHTML = "Game Over";
-	document.getElementById("wordToGuess").innerHTML = "The word to guess was:" + " " + "<strong>" + wordToGuess + "</strong>";
-	document.getElementById("lettersCounter").innerHTML = "";
+	fieldsStatus();
+	document.getElementById("winMessageGO").innerHTML = "Game Over";
+	document.getElementById("wordToGuess").innerHTML = "The word to guess is:" + " " + "<strong>" + wordToGuess + "</strong>";
 }
 
 function resetGame() {
+	fieldsStatus();
 	insertField.disabled = false;
 	check.disabled = false;
-	document.getElementById("win||gameOver").innerHTML = "";
+	document.getElementById("winMessageGO").innerHTML = "";
 	document.getElementById("wordToGuess").innerHTML = "";
-	document.getElementById("lettersCounter").innerHTML = "";
-	document.getElementById("usedLetters").innerHTML = "";
 	document.getElementById("letterCheck").value = "";
 	document.getElementById("printWord").innerHTML = "";
 	letterCounter = 7;
